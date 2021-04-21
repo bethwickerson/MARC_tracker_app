18c005df7348e01223ccb18c0111ce8cba4e9332
Beth Wickerson <beth.wickerson@signaltheory.com>
2021-04-21 09:04:20 -0500

    red status outline calculating, revise download/expand menubar styles, expand screen styles

--- src/App.js
+++ src/App.js
@@ -5,11 +5,6 @@ import LogoTracker from './LogoTracker'
 
 const saveSvgAsPng = require('save-svg-as-png')
 
-const imageOptions = {
-  scale: 10,
-  encoderOptions: 1,
-  backgroundColor: 'white',
-}
 
 
 // The REST API endpoint
@@ -17,9 +12,6 @@ const API_URL = './TotalVaccinatedData.json';
 
 function App() {
 
-  const handleClick = () => {
-    saveSvgAsPng.saveSvgAsPng(document.getElementById('logoTMA'), 'TwoMillionArmsKC.png', imageOptions);
-  };
   // At the beginning, posts is an empty array
   const [posts, setPosts] = useState([]);
 
@@ -37,21 +29,72 @@ function App() {
     return num.toLocaleString('en-US')
   }
 
+  // download options
+  const [dropMenu, showDropMenu] = useState(false);
+  const downloadOption = () => {
+    showDropMenu(!dropMenu);
+    if (embedCode === true) {
+      showEmbedCode(false);
+    }
+  }
+
+  const imageOptions_High = {
+    scale: 25,
+    encoderOptions: 1,
+    backgroundColor: 'white',
+  }
+  const downloadPNG_High = () => {
+    saveSvgAsPng.saveSvgAsPng(document.getElementById('logoTMA'), 'TwoMillionArmsKC.png', imageOptions_High);
+  }
+  const imageOptions_Med = {
+    scale: 15,
+    encoderOptions: 1,
+    backgroundColor: 'white',
+  }
+  const downloadPNG_Med = () => {
+    saveSvgAsPng.saveSvgAsPng(document.getElementById('logoTMA'), 'TwoMillionArmsKC.png', imageOptions_Med);
+  }
+  const imageOptions_Low = {
+    scale: 5,
+    encoderOptions: 1,
+    backgroundColor: 'white',
+  }
+  const downloadPNG_Low = () => {
+    saveSvgAsPng.saveSvgAsPng(document.getElementById('logoTMA'), 'TwoMillionArmsKC.png', imageOptions_Low);
+  }
+  const [embedCode, showEmbedCode] = useState(false);
+  const embedOption = () => {
+    showEmbedCode(!embedCode);
+    if (dropMenu === true) {
+      showDropMenu(false);
+    }
+  }
+
+
   return (
     <>
       <div className="header">
         <div className="button-container">
-          <button onClick={handleClick}>Download  PNG</button>
-          <button>Embed Code</button>
+          <button className={`download-open ${dropMenu === true ? "show" : "hide"}`} onClick={downloadOption}>Download PNG</button>
+          <button className={`embed-open ${embedCode === true ? "show" : "hide"}`} onClick={embedOption}>Embed Code</button>
         </div>
       </div>
       <div className="content">
         {posts.map(post => (
           <LogoTracker
+            percentage={(post.RegimenCompleted_Count / 2000000) * 100}
             count={formatNumber(post.RegimenCompleted_Count)}
             date={post.Date}
           />
         ))}
+      </div>
+      <div className={`download-option ${dropMenu === true ? "show" : "hide"}`}>
+        <button onClick={downloadPNG_Low}>Low Resolution (3500x3500, 658KB)</button>
+        <button onClick={downloadPNG_Med}>Medium Resolution (9300x9300, 3.2MB)</button>
+        <button onClick={downloadPNG_High}>High Resolution (15500x17000, 7.1MB)</button>
+      </div>
+      <div className={`embed-code ${embedCode === true ? "show" : "hide"}`}>
+        lfdsdfldsflsdjfldsaf
       </div>
     </>
   );
